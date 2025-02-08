c171587c693a358d9cd432621ac53b56439fb2ea
Varsha R <varsharamineni@gmail.com>
2025-02-08 13:04:03 +0000

    update file path

--- src/chatbot/main.py
+++ src/chatbot/main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
 import streamlit as st
 from chatbot.model import get_response
